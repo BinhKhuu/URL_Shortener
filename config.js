90d50077b135c40253812e127db40ea22fc97100
Binh Khuu <binhkhuu90@gmail.com>
2017-10-12 15:54:09 +0800

    fixed routing in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 config.db = {};
-config.webhost = 'shrouded-garden-43532/';
+config.webhost = 'shrouded-garden-43532.herokuapp.com/';
 config.db.url = process.env.MONGOLAB_URI;
 
 module.exports = config;
