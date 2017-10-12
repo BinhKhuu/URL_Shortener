2e79515aaa5b6c0254324ba0045b45d7c5d648b8
Binh Khuu <binhkhuu90@gmail.com>
2017-10-12 14:21:46 +0800

    changed config routes

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ config.db = {};
 //config.webhost = 'http://localhost:8080/';
 config.webhost = process.env.MONGOLAB_URI;
 
-config.db.host = 'localhost';
+config.db.host = 'ds117615.mlab.com:17615';
 config.db.name = 'url_shortener';
 
 module.exports = config;
