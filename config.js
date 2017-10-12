248d1d4ded28ee9c895dc80e19dfeb33b65ba31d
Binh Khuu <binhkhuu90@gmail.com>
2017-10-12 14:08:08 +0800

    added mongo hosting

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 var config = {};
 config.db = {};
-config.webhost = 'http://localhost:8080/';
+//config.webhost = 'http://localhost:8080/';
+config.webhost = process.env.MONGOLAB_URI;
 
 config.db.host = 'localhost';
 config.db.name = 'url_shortener';
